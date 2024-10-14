4e1e758c9272b54c6241e4e25f28da10c7a14cff
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2024-10-13 21:13:10 -0500

    012

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const http = require('http')
+const http = require('htpp');
+
 const express = require('express');
 const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio.listen(server);
 
-io.on('connection', socket => {console.log('New user connected');
-});
 
-/*static files*/
 app.use(express.static('public'));
 
-/*starting the server*/
 server.listen(3000, () => {
     console.log('server on port 3000')
 });

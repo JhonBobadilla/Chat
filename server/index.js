cc6171b14ca85db02ed31646106a4a958b840792
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2024-10-14 14:44:46 -0500

    028

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
@@ -11,6 +13,21 @@ const server = createServer(app)
 const io = new Server(server, {connectionStateRecovery: {}
 })
 
+const db = createClient({
+
+url:'libsql://peaceful-juniper-jhonabobadilla.turso.io',
+authToken:'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mjg5MzQxMDYsImlkIjoiNzdiZGU1MDgtNjcwYi00NmQ3LWJhM2EtYzRlYTEzNDI0ZDI0In0.R25w3wcYZ1PAwDAmLkD2D_x90lXzV896_oSzbl0-2EO52PSyhH4JsW9jm-8eMU0A2cVwAeAlVnUrJe75szZeDw'
+})
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+    )
+    `)
+
+
+
 io.on('connection', (socket) => {
     console.log('an user has connected!')
 
@@ -18,7 +35,16 @@ io.on('connection', (socket) => {
     console.log('an user has disconnected!')  
     })
 
-    socket.on('chat message', (msg) => {
+    socket.on('chat message', async (msg) => {
+        let result
+        try {
+          result = await db.execute({
+           sql: `INSERT INTO messages (content) VALUES (:message)`,
+           args: { message: msg }  
+          })
+        }catch (e){
+
+        }
         io.emit('chat message', msg) 
     })
 })
